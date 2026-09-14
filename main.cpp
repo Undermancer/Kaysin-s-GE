36651f2eb0cb8aaf8442571f18b78ce54b03ce37
Undermancer <125559552+Undermancer@users.noreply.github.com>
2026-09-14 23:25:34 +0200

    setup
    ready to code

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <fmt/core.h>
+#include <SDL3/SDL.h>
+
+int main(int argc, char* argv[]) {
+    // Intentamos inicializar el subsistema de video de SDL
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        // Si falla, usamos fmt para imprimir el error exacto que nos da SDL
+        fmt::print("Error crítico: No se pudo inicializar SDL. {}\n", SDL_GetError());
+        return -1;
+    }
+
+    // Si llegamos aquí, la tarjeta gráfica ha respondido bien
+    fmt::print("¡SDL3 inicializado correctamente! El motor gráfico respira...\n");
+
+    // Apagamos SDL limpiamente para no dejar procesos colgados en Windows
+    SDL_Quit();
+    return 0;
+}
